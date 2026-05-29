ec01fa408d19614607359c01f6539fec426d1266
turalgasimov <turallgasimov@gmail.com>
2026-05-30 01:37:30 +0400

    sorted based concurrency

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,4 +8,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """wait_n"""
     tasks = [wait_random(max_delay) for _ in range(n)]
-    return await asyncio.gather(*tasks)
+    return [await task for task in asyncio.as_completed(tasks)]
