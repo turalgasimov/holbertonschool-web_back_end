885d0f51a0ed0c92ca1827713d080054d5c28f4c
turalgasimov <turallgasimov@gmail.com>
2026-05-30 02:40:41 +0400

    got rid of outer loop

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,4 +8,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     '''Async comprehension'''
-    return [_ async for _ in async_generator()]
+    return [i async for i in async_generator()]
