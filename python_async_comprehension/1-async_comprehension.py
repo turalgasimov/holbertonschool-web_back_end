ed6cedd866e0d709edc15e7c3c4eaf986fda5f4c
turalgasimov <turallgasimov@gmail.com>
2026-05-30 02:28:11 +0400

    1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -8,5 +8,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     '''Async comprehension'''
-    for _ in range(10):
+    for _ in range(11):
         return [_ async for _ in async_generator()]
