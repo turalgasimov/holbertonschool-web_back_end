3528652f826e4d2becc6e1f912bf3d6ad28b09da
turalgasimov <turallgasimov@gmail.com>
2026-05-30 01:19:16 +0400

    1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module 1-concurrent_coroutines.py"""
 import asyncio
-from 0-basic_async_syntax import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> float:
