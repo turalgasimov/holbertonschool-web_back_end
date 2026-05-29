97b4937b60fe234f80ecb2ddcdf6f0df02d6d92b
turalgasimov <turallgasimov@gmail.com>
2026-05-30 01:44:50 +0400

    changed function signature

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -5,7 +5,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int = 10) -> float:
+def measure_time(n: int, max_delay: int = 10) -> float:
     """measure_time"""
     start_time = time.time()
     await wait_n(n, max_delay)
