2d8b120cd5e6c648715c18a873268eaefc64b6b1
turalgasimov <turallgasimov@gmail.com>
2026-06-10 17:47:30 -0400

    added docs for 2-hypermedia_pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -25,7 +25,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''Returns the appropriate page of the dataset 
+        '''Returns the appropriate page of the dataset
         (i.e. the correct list of rows)'''
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
