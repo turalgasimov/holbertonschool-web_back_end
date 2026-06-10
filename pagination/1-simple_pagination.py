c35399ff32580a228520f539ee50edb1913c916e
turalgasimov <turallgasimov@gmail.com>
2026-06-10 17:45:10 -0400

    added docs for get_page

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -23,6 +23,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Simple pagination that returns a list of lists'''
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
 
