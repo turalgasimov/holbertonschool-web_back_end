a89b271faabf54df90f8729e8de642cd8e08ce75
turalgasimov <turallgasimov@gmail.com>
2026-06-10 15:59:30 -0400

    2-hypermedia_pagination.py

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+'''Simple pagination'''
+from ast import Dict
+import csv
+from curses import keyname
+import math
+from multiprocessing import Value
+from turtle import st
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        indexes = index_range(page, page_size)
+
+        start = indexes[0]
+        end = indexes[1]
+
+        ds = self.dataset()
+
+        return (ds[start:end])
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, object]:
+        page_data = self.get_page(page, page_size)
+        
+        page_info = {
+            "page_size" : page_size,
+            "page" : page,
+            "data" : page_data,
+            "next_page" : page + 1 if len(page_data) == page_size else None,
+            "prev_page" : page - 1 if page > 1 else None,
+            "total_pages" : math.ceil(len(self.dataset()) / page_size)
+        }
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''simple helper function'''
+    end = page * page_size
+    start = end - page_size
+    return (start, end)
