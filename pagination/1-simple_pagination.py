10e3b92a50ce0a7c33c92e27132d5b177be1b181
turalgasimov <turallgasimov@gmail.com>
2026-06-10 15:17:37 -0400

    test

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -7,8 +7,7 @@ from typing import List
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
