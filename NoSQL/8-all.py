da1bc665743294f19407f93195f6343c413351a4
turalgasimov <turallgasimov@gmail.com>
2026-06-15 17:41:17 +0400

    8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,10 @@
-#!/usr/bin python3
+#!/usr/bin/env python3
 '''Module 8-all'''
+from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
-    return mongo_collection.find()
+    '''Returns a list of all documents in a collection'''
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
