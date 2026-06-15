482c86934cbd9bb07b52fa352480d41dc925fddb
turalgasimov <turallgasimov@gmail.com>
2026-06-15 18:14:47 +0400

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,7 +8,6 @@ def log_stats(mongo_collection):
         print(
             "0 logs\nMethods:\n\tmethod GET: 0\n\tmethod POST: 0\n\tmethod PUT: 0\n\tmethod PATCH: 0\n\tmethod DELETE: 0\n0 status check"
         )
-        return
 
     total_logs = mongo_collection.count_documents({})
     print(f"{total_logs} logs")
