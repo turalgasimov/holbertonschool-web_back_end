6cbf3dc1f88f88b8fc441aec5ce256efb6a63483
turalgasimov <turallgasimov@gmail.com>
2026-06-15 18:13:51 +0400

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,6 +4,11 @@ from pymongo import MongoClient
 
 def log_stats(mongo_collection):
     '''Provides some stats about Nginx logs'''
+    if mongo_collection is None:
+        print(
+            "0 logs\nMethods:\n\tmethod GET: 0\n\tmethod POST: 0\n\tmethod PUT: 0\n\tmethod PATCH: 0\n\tmethod DELETE: 0\n0 status check"
+        )
+        return
 
     total_logs = mongo_collection.count_documents({})
     print(f"{total_logs} logs")
