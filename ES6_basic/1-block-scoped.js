1c71dc6f5785610cef897b6460da0ec71b992746
turalgasimov <turallgasimov@gmail.com>
2026-06-27 11:11:16 +0000

    1-block-scoped.js

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  trueOrFalse = false
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  return [task, task2];
+}
