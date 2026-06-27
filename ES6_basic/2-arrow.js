ddbfa404a71bf643c7c41c212b267ceae2ff76b8
turalgasimov <turallgasimov@gmail.com>
2026-06-27 12:51:07 +0000

    2-arrow.js

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,7 +1,7 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  this.addNeighborhood = newNeighborhood => {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
