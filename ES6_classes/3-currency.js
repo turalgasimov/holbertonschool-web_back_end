00a123ed1f6a1f383347c50c6cdaf38e86a4d736
turalgasimov <turallgasimov@gmail.com>
2026-07-05 10:08:29 +0000

    3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -21,6 +21,6 @@ export default class Currency {
     }
 
     displayFullCurrency() {
-        return `${this._name}` + ` (${this._code})\n`
+        return `${this._name}` + ` (${this._code})`
     }
 }
