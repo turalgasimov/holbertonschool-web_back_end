f3ee668b889a419faa48dcff05589f51aea076e5
turalgasimov <turallgasimov@gmail.com>
2026-07-06 18:10:21 +0400

    4-update_grade_by_city.js

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,16 +2,12 @@ export default function updateGradeByCity(students, city, newGrades) {
     if (!Array.isArray(students) || !Array.isArray(newGrades)) {
         return [];
     }
-    return students.map((student) => {
-        if (student.location === city) {
-            if (!student.hasOwnProperty('grade')) {
-                student.grade = 'N/A';
-            }
-            const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-            if (gradeObj) {
-                return { ...student, grade: gradeObj.grade };
-            }
+
+    return students.fillter((student) => student.location === city).map((student) => {
+        const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+        if (gradeObj) {
+            return { ...student, grade: gradeObj.grade };
         }
-        return student;
+        return { ...student, grade: 'N/A' };
     });
 }
