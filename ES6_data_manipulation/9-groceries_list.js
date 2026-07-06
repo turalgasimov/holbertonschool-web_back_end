f33a765f71482030c52c391d51aec97c9eb171b7
turalgasimov <turallgasimov@gmail.com>
2026-07-06 18:30:55 +0400

    9-groceries_list.js

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const groceries = new Map();
+  groceries.set('Apples', 10);
+  groceries.set('Tomatoes', 5);
+  groceries.set('Pasta', 2);
+  groceries.set('Rice', 1);
+  groceries.set('Bananas', 8);
+
+  return groceries;
+}
