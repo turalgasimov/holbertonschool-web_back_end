8e95c247e508d39a9e681c911e16b774b6f02d84
turalgasimov <turallgasimov@gmail.com>
2026-07-19 12:30:48 +0000

    all done

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,23 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)
+  ]).then((results) => {
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: result.status,
+          value: result.value
+        };
+      } else {
+        return {
+          status: result.status,
+          value: result.reason // Promise.allSettled uses 'reason' for errors
+        };
+      }
+    });
+  });
+}
