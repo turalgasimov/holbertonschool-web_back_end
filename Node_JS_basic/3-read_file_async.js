32a5b4aaa90d2b637d59e80187a537041b8ad92b
turalgasimov <turallgasimov@gmail.com>
2026-07-25 14:29:00 +0400

    update

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -50,7 +50,7 @@ function countStudents(path) {
 
       for (const [field, names] of Object.entries(fields)) {
         console.log(
-          `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`
+          `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`,
         );
       }
 
